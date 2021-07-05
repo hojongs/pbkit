6eb90cf8070ee8683a68fb57fd08dfa74217f2eb
Changseo Jang <changseo.jang@riiid.co>
2021-07-05 14:36:06 +0900

    add enum

--- codegen/kt/index.ts
+++ codegen/kt/index.ts
@@ -1,10 +1,30 @@
-import { Schema } from "../../core/schema/model.ts";
+import { EnumField, Schema } from "../../core/schema/model.ts";
 import { CodeEntry } from "../index.ts";
+import { Enum, Message } from "../../core/schema/model.ts";
 
 import { StringReader } from "https://deno.land/std@0.98.0/io/mod.ts";
 
 function buildFilePathFromPackageName(packageName: string) {
   return `${packageName.split(".").join("/")}.kt`;
+}
+
+function genEnum(typeName: string, type: Enum): string {
+  const fields = Object.entries<EnumField>({
+    "0": { description: "", name: "UNSPECIFIED", options: {} },
+    ...type.fields,
+  });
+  const enumString = `public enum class ${typeName}(val value: Int) {\n${
+    fields.map(([fieldValue, { name }]) => `  ${name}(${fieldValue}),`).join(
+      "\n",
+    )
+  }
+}`;
+
+  return enumString;
+}
+
+function genMessage(typeName: string, type: Message): string {
+  return `MSG_${typeName}_UNIMPLEMENTED`;
 }
 
 export default async function* gen(
@@ -13,8 +33,8 @@ export default async function* gen(
   const files = schema.files;
 
   for (const filename in files) {
-    console.log(files[filename]);
-    const packageName = files[filename].options.java_package;
+    const file = files[filename];
+    const packageName = file.options.java_package;
     if (typeof packageName !== "string") {
       console.log("Java package not found!");
       continue;
@@ -22,9 +42,29 @@ export default async function* gen(
 
     const packageString = `package ${packageName}`;
 
+    const typePaths = file.typePaths;
+    const typeString = typePaths.map((typePath) => {
+      const type = schema.types[typePath];
+      const typeName = typePath.split(".").slice(-1)[0];
+
+      switch (type.kind) {
+        case "enum":
+          return genEnum(typeName, type);
+        case "message":
+          return genMessage(typeName, type);
+        default:
+          console.error("Unknown type");
+          throw Error("Undefined type");
+      }
+    }).join("\n\n");
+
     yield [
       buildFilePathFromPackageName(packageName),
-      new StringReader([packageString].join("\n")),
+      new StringReader([
+        packageString,
+        "",
+        typeString,
+      ].join("\n")),
     ];
   }
 }
